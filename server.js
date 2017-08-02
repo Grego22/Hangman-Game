6d5b4f56e1cd90dbf09a7e9c0065aab41d0192a6
Greg Garville Jr <gjgarvil@gmail.com>
2017-08-02 14:27:03 -0400

    running notes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
-const sessions = require('express-sessions')
+const session = require('express-session')
 const fs = require('fs')
 
 const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n")
