9cf3911775c18fd4474be96f30941ed1ee6cab5e
Greg Garville Jr <gjgarvil@gmail.com>
2017-08-02 14:14:09 -0400

    generating random #

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
+const sessions = require('express-sessions')
+const fs = require('fs')
+
+const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n")
 
 const app = express()
 
@@ -10,3 +14,8 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.engine('mustache', mustacheExpress())
 app.set('views', './views')
 app.set('view engine', 'mustache')
+
+/* Seems the main challenge in this assignment is how to generate a random word
+A for loop would most likely be the best way of going about this.
+
+after getting the random word, have to figure out a method to break the word down into letters.
