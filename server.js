46958b4bff87319c45ca2b40a2bf374101c01bf9
Greg Garville Jr <gjgarvil@gmail.com>
2017-08-04 16:11:19 -0400

    working on server.js

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require('express')
 const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
-const session = require('express-session')
+const expressSession = require('express-session')
 const fs = require('fs')
 
 const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n")
 
 const app = express()
 
-// app.use(
-//   expressSession({
-//     secret: "keyboard cat",
-//     resave: false,
-//     saveUninitialized: true
-//   })
+app.use(
+  expressSession({
+    secret: "kinky wizards",
+    resave: false,
+    saveUninitialized: true
+  }))
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: false}))
@@ -23,34 +23,28 @@ app.set('views', './views')
 app.set('view engine', 'mustache')
 
 // random number
-let rando = Math.floor(Math.random() * words.length)
+let randomWord = words[Math.floor(Math.random() * words.length)]
 
 
-/* Seems the main challenge in this assignment is how to generate a random word
-A for loop would most likely be the best way of going about this.
-after getting the random word, have to figure out a method to break the word down into letters.
-*/
-for (var i = 0; i < words.length; i++) {
-  words[i]
 
-let wordlength = words[i]
-console.log(wordlength);
+//for loop for generating individual letters of the randomWord
+for (var i = 0; i < randomWord.length; i++) {
+  randomWord[i]
+  let mysteryWord = randomWord[i]
+  console.log(mysteryWord);
 }
-
 // Seems like another challenge is express sessions. Ive been  reading up on it, but
 // not very comfortable with it right now.  Think it is integral in storing a user's
 // "guessed letters "
 
-// app.get('/', (request, response) =>{
-//
-//   response.render('homepage', {todoListForTheBrowser: taskList, completedTasksForTheBrowser: completedTasks})
-//   // this is confusing, have to revisit
-// })
-// app.post('/markComplete', (request, response)=>{
-//   console.log(request.body)
-//   response.send('marking something complete')
+app.get('/', (request, response) =>{
 
-
+  response.render('index')
+})
+app.post('/guess', (request, response)=>{
+  console.log(request.body)
+  response.send('marking something complete')
+})
 
 // also going to need an if statement that prints out if the player wins/loses.
 // Furthermore a count function that lets the player (user) know
